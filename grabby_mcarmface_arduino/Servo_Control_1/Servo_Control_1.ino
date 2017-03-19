ec8b4583a3e602a699463d3afed1f7825e0b6dac
Rico5678 <ewells@ualberta.ca>
2017-03-18 18:09:49 -0700

    added the most lit branch ever m8888s git it

---
+++ grabby_mcarmface_arduino/Servo_Control_1/Servo_Control_1.ino
@@ -0,0 +1,42 @@
+// Torso: 0-180
+//        0-210 degrees
+//       analog 75-632
+
+// Shoulder: 0-160
+//           theta1 vertical = 80
+//            analog 68-560
+// Elbow: 0-180
+//        theta2 straight = 110 
+//        analog 93-483       
+
+// Wrist: 0-180
+//        no limits
+//        analog 89-474
+
+
+// Pinch: 0-180
+//        75-100-170
+//        263-460
+
+
+
+void setup() {
+  Serial.begin(115200);
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+
+}
+
+void serial_read(){
+
+  if (Serial.available()>0){
+    
+  }
+}
+
